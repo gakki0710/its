0400a0f7a122f0fc42de2e4c4236381490481f2e
gakki007 <gakki0710@yahoo.co.jp>
2023-04-13 17:02:40 +0900

    初回コミット

---
+++ src/main/java/com/example/its/application/service/IssueService.java
@@ -0,0 +1,29 @@
+package com.example.its.application.service;
+
+import com.example.its.domain.model.IssueEntity;
+import com.example.its.domain.repository.IssueRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class IssueService {
+
+    private final IssueRepository issueRepository;
+
+    public List<IssueEntity> findAll() {
+        return issueRepository.findAll();
+    }
+
+    @Transactional
+    public void create(String summary, String description) {
+        issueRepository.insert(summary, description);
+    }
+
+    public IssueEntity findById(long issueId) {
+        return issueRepository.findById(issueId);
+    }
+}
